23e36a2709f8b3ba0c752db5892ebd6d31f926b1
Kriso <kriselveliev@abv.bg>
2018-03-30 13:25:07 +0300

    UserDAO singleton

--- TechnoMarket/src/dao/UserDAO.java
+++ TechnoMarket/src/dao/UserDAO.java
@@ -1,5 +1,20 @@
 package dao;
 
 public class UserDAO {
+	
+	private static UserDAO instance;
+	
+	private UserDAO() {
+			
+	}
+	
+	public static  synchronized UserDAO getInstance() {
+		
+		if(instance==null) {
+			instance=new UserDAO();
+		}
+		return instance;	
+	}
+	
 
 }
