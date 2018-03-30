87f3a70060cbb6883f066bce4cb10dbf02f42b34
Kriso <kriselveliev@abv.bg>
2018-03-30 12:39:00 +0300

    DBManager filling still more to come

--- TechnoMarket/src/controller/DBManager.java
+++ TechnoMarket/src/controller/DBManager.java
@@ -1,5 +1,31 @@
 package controller;
 
+import java.sql.Connection;
+
 public class DBManager {
+	
+	private static Connection connection;
+	private static DBManager instance;
+	
+	public synchronized static DBManager getInstance() {
+		
+		if(instance==null) {
+			instance=new DBManager();
+		}
+		return instance;
+		
+	}
+	
+	
+	private DBManager() {
+		//connection here
+	}
+	
+	
+	public Connection getConnection() {
+		
+		return connection;
+		
+	}
 
 }
